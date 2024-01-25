7999b759733f850722796d83a72423eb9a3858e9
Daniel Ximenez <danixidev@gmail.com>
2024-01-25 14:12:59 +0100

    Crear interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface Round {
+  index: number,
+  first: number,
+  second: number,
+  third: number,
+  total: number
+}
+
+interface Player {
+  name: string,
+  color: string,
+  rounds: Round[],
+  last_score: number
+}
+interface Game {
+  round: number,
+  totalScore: number,
+  players: Player[]
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
